8fc78828d190e53478ea87c92fe1ab360ef16f2c
Marcelo Leal <andradefj13@hotmail.com>
2026-03-24 16:24:48 -0300

    d
    d

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FinanceModule } from './finance/finance.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [FinanceModule],
+  imports: [
+    FinanceModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
