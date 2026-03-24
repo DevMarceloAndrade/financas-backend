9e6454a7a0a3d6b883142297fa005fd08934cc5b
Marcelo Leal <andradefj13@hotmail.com>
2026-03-24 16:16:43 -0300

    Update prisma.service.ts

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -4,14 +4,14 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
     constructor() {
-    super({
-      datasources: {
-        db: {
-          url: 'file:./dev.db', // Use o mesmo nome (db) que está no seu schema.prisma
-        },
-      },
-    });
-  }
+        super({
+            datasources: {
+                db: {
+                    url: 'file:./dev.db', // Use o mesmo nome (db) que está no seu schema.prisma
+                },
+            },
+        });
+    }
     async onModuleInit() {
         await this.$connect();
     }
