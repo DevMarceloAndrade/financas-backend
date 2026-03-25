0ecfa6a239fdcae17a54a7cd80a777055ef33228
Marcelo Leal <andradefj13@hotmail.com>
2026-03-25 10:33:54 -0300

    x

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,12 +1,21 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { createClient } from '@libsql/client';
+import { PrismaLibSql } from '@prisma/adapter-libsql';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
     constructor() {
-        super({
-            
-        }); // 👈 remove o datasources
+        // 1. Cria o cliente do banco de dados (libsql funciona com SQLite)
+        const libsql = createClient({
+            url: process.env.DATABASE_URL as string,
+        });
+
+        // 2. Cria o adaptador para o Prisma
+        const adapter = new PrismaLibSql(libsql);
+
+        // 3. Passa o adaptador para o construtor do PrismaClient
+        super({ adapter });
     }
 
     async onModuleInit() {
